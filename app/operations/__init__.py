71cb37ce5ef903d71e38dee3cf22e931bf908130
GabJuarez <gajuarez@uamv.edu.ni>
2025-10-25 12:20:37 -0600

    rehaciendo el main para hacer la interfaz con fask

--- app/operations/__init__.py
+++
@@ -1,4 +0,0 @@
-from .utils import normalizar_ecuacion, agregar_multiplicacion_implicita
-from .eliminacion_gaussiana import crear_matriz as crear_matriz_gauss, eliminacion_filas, imprimir_soluciones
-from .gauss_jordan import crear_matriz as crear_matriz_jordan, gauss_jordan, imprimir_resultados
-
