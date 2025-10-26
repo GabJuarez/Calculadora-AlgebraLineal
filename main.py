fb684fdca8e2ad7d4ff68de1c7760d24fbc65399
smejiaa19 <silviomejiagarcia@gmail.com>
2025-10-26 16:26:02 -0600

    Hola probando Flask

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__,
+            template_folder='app/templates',
+            static_folder='app/static')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/gauss-jordan')
+def gaussJordan():
+    return render_template('Gauss-Jordan.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
