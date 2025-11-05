e6de42ac2c47bdbf95ad31a93a5467ac59895258
GabJuarez <gajuarez@uamv.edu.ni>
2025-11-04 21:53:29 -0600

    exponiendo funcion necesaria para la biseccion

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from .logic import comprobar_independencia_lineal
 from .logic import eliminacion_gaussiana
 from .logic import matriz_triangular, calcular_determinante
 from. logic import traspuesta
+from .logic import biseccion
 from .web_utils import matriz_desde_formulario
 
 
