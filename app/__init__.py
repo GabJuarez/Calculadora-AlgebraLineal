84bd3655a478d4a362eb91e101550f1e70a9b1c1
GabJuarez <gajuarez@uamv.edu.ni>
2025-11-12 13:51:20 -0600

    exponiendo funciones del paquete
    Co-authored-by: Silvio Mejia <silviomejiagarcia@gmail.com>
    Co-authored-by: G. Juarez <gajuarez@uamv.edu.ni>

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,10 @@ from .logic import gauss_jordan_pasos
 from .logic import comprobar_independencia_lineal
 from .logic import eliminacion_gaussiana
 from .logic import matriz_triangular, calcular_determinante
-from. logic import traspuesta
+from .logic import traspuesta
 from .logic import biseccion
+from .logic import falsa_posicion
+from .logic import operar_matrices
 from .web_utils import matriz_desde_formulario
-
-
+from .web_utils import generate_preview_plot_for_function
+from .web_utils import generar_grafico_por_defecto
